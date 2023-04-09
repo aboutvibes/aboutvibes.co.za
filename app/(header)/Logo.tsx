de976ae97056821010c700492a63628446cdd89b
Tshepo <aboutvibespty@gmail.com>
2023-04-09 17:58:34 +0200

    First commit

--- app/(header)/Logo.tsx
+++ app/(header)/Logo.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import companyLogo from "../../public/img/logo/dark_logo_transparent_cozxhf.webp";
+import Image from "next/image";
 
 const Logo = () => {
   return (
     <Link href={"/"}>
-      <img
-        src="https://res.cloudinary.com/ddovp5ssb/image/upload/v1679957532/AB%20Photos/Logo/dark_logo_transparent_cozxhf_v5j29s.webp"
+      <Image
+        src={companyLogo}
         alt="Company Logo"
         width={140}
         height={140}
